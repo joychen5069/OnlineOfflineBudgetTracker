cf5ae12d9bf8f4cd16259cd602d8b7c8e625989b
Joy Chen <joychen5069@gmail.com>
2020-06-25 19:48:16 -0400

    added manifest and service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,20 +1,64 @@
-{
-    "name": "Notetaker",
-        "short_name": "Notetaker",
-            "icons": [
-                {
-                    "src": "assets/images/icons/icon-72x72.png",
-                    "sizes": "72x72",
-                    "type": "image/png"
-                },
-                {
-                    "src": "assets/images/icons/icon-96x96.png",
-                    "sizes": "96x96",
-                    "type": "image/png"
-                },
-            ],
-                "theme_color": "#ffffff",
-                    "background_color": "#ffffff",
-                        "start_url": "/",
-                            "display": "standalone"
-}
+const FILES_TO_CACHE = [
+    "/",
+ "/index.html", 
+ "index.js", 
+ "db.js",
+ "style.css"
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// install
+self.addEventListener("install", function(evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+// activate
+self.addEventListener("activate", function(evt) {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+// fetch
+self.addEventListener("fetch", function(evt) {
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
+
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(evt.request);
+          });
+      }).catch(err => console.log(err))
+    );
+
+    return;
+}});
